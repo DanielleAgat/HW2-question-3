6a9060729553e48a040d0e0bed124f1f4b6a1315
Agat Levi <agat.levi@ridewithvia.com>
2020-04-03 19:09:27 +0300

    inserted given main function

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 //Student's name: Danielle Agat Levi
 //Student's ID: 313357329
 
+/* This program gets from the user strings array and an array of cells.
+ * Then it removes those cells from the strings array and prints the array after the changes.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,6 +25,21 @@ char** setPtrToCharsArray(char** str_array){
     return res;
 }
 
+//This function gets a pointer to an array of strings and an array of pointers to chars in those string, and removes the chars of the pointers array.
+unsigned int RemoveFromStrArray(char ***str_array, unsigned int str_array_size, char** ptr_to_chars_array);
+
+//This function gets a number of strings from the user, separated by ENTER, and returns an array of strings.
+char** getStrArrayInput(int* size);
+
+//This function gets an str array and prints it.
+void printArray(char** str_array,int size);
+
+//This function prints a given string.
+void println(char* line);
+
+//This function frees a strings array.
+void freeArray(char** str_array,int size);
+
 
 int main(){
     char** str_array;
@@ -38,3 +57,31 @@ int main(){
     free(ptr_to_chars_array);
 }
 
+
+unsigned int RemoveFromStrArray(char ***str_array, unsigned int str_array_size, char** ptr_to_chars_array){
+
+}
+
+
+char** getStrArrayInput(int* size){
+
+}
+
+
+void printArray(char** str_array,int size){
+    for(int i=0; i<size; i++){
+        println(str_array[i]);
+    }
+}
+
+void println(char* line){
+    for(int i=0; line[i] != '\0'; i++){
+        printf(line[i]);
+    }
+}
+
+void freeArray(char** str_array,int size){
+    for(int i=(size-1); i >=0 ; i--){
+        free(str_array[i]);
+    }
+}
