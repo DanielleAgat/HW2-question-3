968339f1243b497bf1b175193ba3a26ea727aca9
Agat Levi <agat.levi@ridewithvia.com>
2020-04-03 18:36:22 +0300

    inserted given main function

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
-#include <stdio.h>
+//Student's name: Danielle Agat Levi
+//Student's ID: 313357329
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+char** setPtrToCharsArray(char** str_array){
+    char** res;
+    int size, i;
+    int str_array_row, str_array_col;
+
+    scanf("%d", &size); // Get number of ptrs
+    res = (char**)malloc(sizeof(char*)*(size+1)); // Add 1 for NULL at the end
+
+    for (i = 0; i < size; i++){
+        scanf("%d", &str_array_row);
+        scanf("%d", &str_array_col);
+        res[i] = str_array[str_array_row] + str_array_col;
+    }
+    res[size] = NULL; // Set the last one to NULL
+    return res;
 }
+
+
+int main(){
+    char** str_array;
+    unsigned int str_array_size;
+    char** ptr_to_chars_array;
+    unsigned int res;
+
+    str_array = getStrArrayInput(&str_array_size); // Get the size and strings from user (can't assume max size for each string)
+    ptr_to_chars_array = setPtrToCharsArray(str_array);
+    res = RemoveFromStrArray(&str_array, str_array_size, ptr_to_chars_array);
+    printArray(str_array, str_array_size - res);
+
+    // Free memory
+    freeArray(str_array, str_array_size - res);
+    free(ptr_to_chars_array);
+}
+
