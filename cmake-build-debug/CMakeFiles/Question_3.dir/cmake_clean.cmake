4b2998067e9eea3e5c830a76039830937be7dba7
Agat Levi <agat.levi@ridewithvia.com>
2020-04-03 18:27:33 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Question_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Question_3.dir/main.c.o"
+  "Question_3"
+  "Question_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Question_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
