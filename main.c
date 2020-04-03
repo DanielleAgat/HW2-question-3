9d818d81e50572520d9f117ab763e5ee34187e2b
Agat Levi <agat.levi@ridewithvia.com>
2020-04-03 19:32:28 +0300

    inserted given main function

--- main.c
+++ main.c
@@ -8,6 +8,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define END_OF_STR '\0'
+#define ENTER '\n'
+
 char** setPtrToCharsArray(char** str_array){
     char** res;
     int size, i;
@@ -29,7 +32,10 @@ char** setPtrToCharsArray(char** str_array){
 unsigned int RemoveFromStrArray(char ***str_array, unsigned int str_array_size, char** ptr_to_chars_array);
 
 //This function gets a number of strings from the user, separated by ENTER, and returns an array of strings.
-char** getStrArrayInput(int* size);
+char** getStrArrayInput(int* pSize);
+
+//This function scans a line of strings.
+char* scanLine();
 
 //This function gets an str array and prints it.
 void printArray(char** str_array,int size);
@@ -39,6 +45,9 @@ void println(char* line);
 
 //This function frees a strings array.
 void freeArray(char** str_array,int size);
+
+//This method checks if memory allocation succeded.
+void checkIfMallocSucceeded(void* ptr);
 
 
 int main(){
@@ -63,25 +72,68 @@ unsigned int RemoveFromStrArray(char ***str_array, unsigned int str_array_size, 
 }
 
 
-char** getStrArrayInput(int* size){
+char** getStrArrayInput(int* pSize){
+    int size;
+    scanf("%d",&size);
 
+    char** str_array = (char**)malloc(sizeof(char*) * size);
+    checkIfMallocSucceeded(str_array);
+
+    for(int i=0; i<size; i++){
+        str_array[i] = scanLine();
+    }
 }
 
+char* scanLine(){
+    int phySize = 2,logSize=0;
+    char* line = (char*)malloc(sizeof(char)*phySize);
+    checkIfMallocSucceeded(line);
+
+    char scanned = getchar();
+    while(scanned != ENTER ){ //scans a string until Enter is being scanned
+        if(logSize < phySize){
+            line[logSize++] = scanned;
+            scanned = getchar();
+        }else{
+            line = (char*)realloc(line,sizeof(char)*phySize * 2);
+            checkIfMallocSucceeded(line);
+            phySize *= 2;
+        }
+    }
+
+    if(phySize > logSize){
+        line[logSize++] = END_OF_STR;
+        if(phySize > logSize) {
+            line = (char*)realloc(line, sizeof(char) * logSize);
+            checkIfMallocSucceeded(line);
+        }
+    }else{
+        line = (char*)realloc(line,sizeof(char)*(++logSize));
+        checkIfMallocSucceeded(line);
+        line[logSize] = END_OF_STR;
+    }
+
+    return line;
+}
 
 void printArray(char** str_array,int size){
-    for(int i=0; i<size; i++){
+    for(int i=0; i<size; i++)
         println(str_array[i]);
-    }
 }
 
 void println(char* line){
-    for(int i=0; line[i] != '\0'; i++){
+    for(int i=0; line[i] != '\0'; i++)
         printf(line[i]);
-    }
 }
 
 void freeArray(char** str_array,int size){
-    for(int i=(size-1); i >=0 ; i--){
+    for(int i=(size-1); i >=0 ; i--)
         free(str_array[i]);
+}
+
+void checkIfMallocSucceeded(void* ptr){
+    if(ptr == NULL){
+        printf("ERROR: Out of memory!\n");
+        exit(1);
     }
 }
