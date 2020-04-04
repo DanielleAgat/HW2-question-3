d9ca974ffe69133dbe3c839bfd99c6e1599bc2af
Agat Levi <agat.levi@ridewithvia.com>
2020-04-04 15:42:45 +0300

    Done after tests

--- main.c
+++ main.c
@@ -17,6 +17,19 @@ char** setPtrToCharsArray(char** str_array);
 //This function gets a pointer to an array of strings and an array of pointers to chars in those string, and removes the chars of the pointers array.
 unsigned int RemoveFromStrArray(char ***str_array, unsigned int str_array_size, char** ptr_to_chars_array);
 
+//This function replace all values of a given pointers array into ENTER.
+int replaceArrWithFlags(char** ptr_to_chars_array);
+
+//This function gets an array of strings and removes the empty strings from it.
+char** removeEmptyCells(char** str_array, unsigned int str_array_size);
+
+//This function gets an array of strings and removes all the '\n' chars from it.
+// If there's left empty strings then it returns the amount.
+int removeFlagsFromArr(char** str_array, unsigned int* pSize);
+
+//This function gets a string and removes all the '\n' chars in it
+int removeFlagsFromStr(char* str , unsigned int* pSize);
+
 //This function gets a number of strings from the user, separated by ENTER, and returns an array of strings.
 char** getStrArrayInput(int* pSize);
 
@@ -54,7 +67,67 @@ int main(){
 
 
 unsigned int RemoveFromStrArray(char ***str_array, unsigned int str_array_size, char** ptr_to_chars_array){
-    return 0;
+    replaceArrWithFlags(ptr_to_chars_array);
+    unsigned int numOfRemovedCells = removeFlagsFromArr(*str_array,&str_array_size);
+    *str_array = removeEmptyCells(*str_array,str_array_size);
+    return numOfRemovedCells;
+}
+
+int replaceArrWithFlags(char** ptr_to_chars_array){
+    int size = 0;
+    for(int i=0; ptr_to_chars_array[i] != NULL ; i++){
+        *(ptr_to_chars_array[i]) = ENTER; // Changes the pointer's value to '\n' (since we know '\n' will never appear inside our given strings
+        size++;
+    }
+    return size;
+}
+
+char** removeEmptyCells(char** str_array, unsigned int str_array_size){
+    int index = 0;
+    char** temp = (char**)malloc(str_array_size * sizeof(char*));
+    checkIfMallocSucceeded(temp);
+    for(int i=0; index < str_array_size ; i++){
+        if(*(str_array[i]) != END_OF_STR){
+            temp[index] = str_array[i];
+//            println(temp[index]);
+            index++;
+        }
+    }
+    free(str_array);
+    return temp;
+}
+
+int removeFlagsFromArr(char** str_array, unsigned int* pSize){
+    int size = *pSize, strSize, removedFlagsStr,emptyCells = 0;
+    for(int i=0 ; i < size ; i++){
+        removedFlagsStr = removeFlagsFromStr(str_array[i],&strSize);
+        if(strSize == 0){ // Meaning there is only one char - the EOF char
+            emptyCells++;
+        }else if(removedFlagsStr != 0) { //If the array's size was changes then reallocate memory
+            str_array[i] = (char*)realloc(str_array[i],sizeof(char)*strSize);
+            checkIfMallocSucceeded(str_array[i]);
+        }
+    }
+    *pSize = (size - emptyCells);
+    return emptyCells;
+}
+
+int removeFlagsFromStr(char* str , unsigned int* pSize){
+    int removedFlags = 0, validChars = 0, index =0;
+
+    for(int i=0; str[index] != END_OF_STR ; i++,index++){
+        while(str[i] == ENTER){ //Skip all flags
+            removedFlags++;
+            i++;
+        }
+        if(str[i] != END_OF_STR){
+            validChars++;
+        }
+        if(removedFlags != 0) //insert a valid char into the temp array
+            str[index] = str[i];
+    }
+    *pSize = validChars;
+    return removedFlags;
 }
 
 
